d8fa8e363664cab56acf4b1cd037314c1e8c21a6
guilhermegma <guilhermegmlandrade@gmail.com>
2026-06-11 07:55:27 -0300

    chore: remove unused comments and clean up code

--- ed1-cpp/data-structures/ordered-binary-tree.cpp
+++ ed1-cpp/data-structures/ordered-binary-tree.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class Node {
-public: // CORREÇÃO SINTÁTICA: Necessário para a classe tree acessar os membros
+public: 
     int info;
     Node *left, *right;
 };
@@ -14,16 +14,14 @@ public:
    tree() {
     root = nullptr;
    }
-   // CORREÇÃO SINTÁTICA: Mudado de void para Node* para aceitar os seus "return(root)"
    Node* insert(Node *root, int n); 
    Node *remove(Node *root, int n);
    Node *searchSuccessor(Node *sus, int n, Node *root);
 };
 
-// CORREÇÃO SINTÁTICA: Mudado para Node* para casar com o retorno interno da sua lógica
 Node* tree::insert (Node *root, int n) {
     if (root == nullptr) {
-        root = new Node; // CORREÇÃO SINTÁTICA: Alocação correta em C++ (era newNode())
+        root = new Node;
         root->info = n;
         root->left = nullptr;
         root->right = nullptr;
@@ -39,22 +37,16 @@ Node* tree::insert (Node *root, int n) {
         return (root);
     }
 }
-
-// CORREÇÃO SINTÁTICA: Adicionado o escopo "tree::" e ajustado os parâmetros conforme declarado na classe
 Node* tree::remove (Node *root, int n) {
     Node *aux;
-    // Mantida a assinatura da classe. Se precisar da assinatura com (Node* root, int n), 
-    // lembre-se de alterar também a declaração dela dentro da classe 'tree' lá em cima.
     if (root == nullptr) {
         return root;
     } 
     if (root->info < n) {
-        // Como o método da classe não recebe o nó como parâmetro, a recursão foi mantida 
-        // exatamente como você escreveu, mas gerará um alerta de lógica no compilador.
         root->right = remove(root->right, n); 
     }
     else if (root->info > n) {
-        root->left = remove(root->left, n); // CORREÇÃO SINTÁTICA: Adicionado o ponto e vírgula ";" faltando
+        root->left = remove(root->left, n);
     } 
     else if (root->left == nullptr) {
         aux = root;
